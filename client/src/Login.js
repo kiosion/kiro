5ec0924a5870e50a0ab11675f9954adeecb0c6ac
toastythetoaster <grwdragon@gmail.com>
2021-08-27 19:58:53 -0400

    Added electron to client <3

--- client/src/Login.js
+++ client/src/Login.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Container } from "react-bootstrap"
 
 const client_id = '63fed822c0e24c0d9e54350fa259fdf1'
-const redirect_uri = 'https://kiro.kio.dev/'
+const redirect_uri = 'http://localhost:3000'
 const scopes = 'streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}&scope=${scopes}`
 
